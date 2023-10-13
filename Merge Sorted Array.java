9c90406b37f13b4f8bbff7d4152a1cacb0fa9951
Ahmed Assaf <70029112+AhmedAssaf32@users.noreply.github.com>
2023-10-13 14:33:01 +0300

    Update Merge Sorted Array.java

--- Merge Sorted Array.java
+++ Merge Sorted Array.java
@@ -1,24 +1,7 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-       /* for(int i = 0 < nums1.length()-1; i++){
-        }*/
-        /*int tmp;//, j=0;
-        for(int i=0; i<(m+n);i++ ){
-            for(int j=0; j<n; ){
-              //while(j<n){
-              if(nums1[i] == 0){
-                  tmp = nums1[i];
-                  nums1[i] = nums2[j];
-                //nums2[j] = tmp;
-                //j++;
-            }
-            j++;*/
-              /*while(j<n){
-                j++;
-              }*/
-            /*}
-        }*/
-        int i = m - 1;  // Index for nums1, initialized to the last element of nums1
+
+                int i = m - 1;  // Index for nums1, initialized to the last element of nums1
         int j = n - 1;  // Index for nums2, initialized to the last element of nums2
         int k = m + n - 1;  // Index for the merged array, initialized to the last position
 
@@ -42,6 +25,28 @@ class Solution {
 
     }
 }
+
+//class Solution {
+       /* for(int i = 0 < nums1.length()-1; i++){
+        }*/
+        /*int tmp;//, j=0;
+        for(int i=0; i<(m+n);i++ ){
+            for(int j=0; j<n; ){
+              //while(j<n){
+              if(nums1[i] == 0){
+                  tmp = nums1[i];
+                  nums1[i] = nums2[j];
+                //nums2[j] = tmp;
+                //j++;
+            }
+            j++;*/
+              /*while(j<n){
+                j++;
+              }*/
+            /*}
+        }*/
+   // }
+//}
 
 /*
 class Solution {
