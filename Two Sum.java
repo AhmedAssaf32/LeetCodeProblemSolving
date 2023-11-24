65d27cf733e84d31b784d43d5be885c55f490ebd
Ahmed Assaf <70029112+AhmedAssaf32@users.noreply.github.com>
2023-11-24 15:42:50 +0200

    Update Two Sum.java
    add another solution using hash set

--- Two Sum.java
+++ Two Sum.java
@@ -1,6 +1,6 @@
 //Two Sum
 
-class Solution {
+/*class Solution {
     public int[] twoSum(int[] nums, int target) {
         
         //int [] res;
@@ -16,4 +16,49 @@ class Solution {
         }
         return null;
     }
+}*/
+
+/*another solutions*/
+/*class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i-1] + nums[i] == target)
+                return new int[] {i-1,i};
+        }
+        return null;
+    }
+}*/
+/*class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        //int [] res;
+        for(int i = 0; i< nums.length;i++){
+            for(int j = i+1; j< nums.length;j++){
+              if(nums[i] + nums[j] == target){
+                //res[] = {i,j};
+                //return res;
+                return new int[] { i, j };
+                //return new int[] { i, i+1 };
+             }
+            }
+        }
+        return null;
+    }
+}*/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        Map <Integer , Integer> newArr = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if(newArr.containsKey(complement)){
+                return new int[]{newArr.get(complement),i};
+            }
+            newArr.put(nums[i],i);
+        }
+        return null;
+
+    }
 }
+    
